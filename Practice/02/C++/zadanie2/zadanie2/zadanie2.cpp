80da1493555cb8f496827a9ab86ee5b5ecadbc95
BORGER <borger-2018@mail.ru>
2020-10-01 18:28:45 +0300

    commit 5

---
+++ Practice/02/C++/zadanie2/zadanie2/zadanie2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    double c;
+    double d;
+    (a = 3);
+    (b = 3.14); //Будет выводится "3", т.к. это целочисленная переменная
+    (c = 3);
+    (d = 3.14);
+   cout << a << endl;
+   cout << b << endl;
+   cout << c << endl;
+   cout << d << endl;
+}
