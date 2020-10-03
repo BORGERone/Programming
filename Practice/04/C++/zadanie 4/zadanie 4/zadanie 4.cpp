277f84638e783388284aee297ce18552edacc450
BORGER <borger-2018@mail.ru>
2020-10-03 11:56:57 +0300

    Davai

---
+++ Practice/04/C++/zadanie 4/zadanie 4/zadanie 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a;
+    double b;
+
+    cout << "Введите значение a: ";
+    cin >> a;
+    cout << "Введите значение b: ";
+    cin >> b;
+
+    swap(a, b);
+
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+    (a, b = b, a);
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+}
