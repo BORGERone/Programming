76b5e22359415344d9ff047ec335a7b040c08afa
BORGER <borger-2018@mail.ru>
2020-10-05 17:11:58 +0300

    commit 5

--- Practice/06/C++/zadanie 6/zadanie 6/zadanie 6.cpp
+++ Practice/06/C++/zadanie 6/zadanie 6/zadanie 6.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
     double a;
-	double b;
-	double c;
-	double x;
+    double b;
+    double c;
+    double x;
 
     cout << "Введите значение a: ";
     cin >> a;
@@ -16,37 +16,39 @@ int main() {
     cout << "Введите значение c: ";
     cin >> c;
 
-    if ((a + b + c) == 0)
-    {
-        cout << "Бесконечно корней  ＼(º □ º *)/" << endl;
-    }
+    if (a == 0)
+        if (b == 0)
+            if (c == 0)
+            {
+                cout << "Бесконечно корней " << endl;
+                return 0;
+            }
+
 
     if (a == 0)
         if (b != 0)
             if (c != 0)
-    {
-        x = (b / -c);
-        cout << "x1 = " << x << endl;
-    }
-    
-        if ((b * b - 4 * a * c) > 0)
-            if (a != 0)
+            {
+                x = (b / -c);
+                cout << "x1 = " << x << endl;
+                return 0;
+            }
+
+
+    if ((b * b - 4 * a * c) > 0)
+        if (a != 0)
         {
             x = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
             cout << "x1 = " << x << endl;
             x = (-1 * b - sqrt(b * b - 4 * a * c)) / (2 * a);
             cout << "x2 = " << x << endl;
+            return 0;
         }
-   
-        if ((b * b - 4 * a * c) < 0)
+
+    if ((b * b - 4 * a * c) < 0)
+        if (a != 0)
         {
-            x = (b * b - 4 * a * c);
-            cout << "x 1 = " << x << endl;
-        }
-        if ((b * b - 4 * a * c) == 0)
-            if (a != 0)
-        {
-            x = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
-            cout << "x1 = " << x << endl;
+            cout << "Решений нет" << endl;
+            return 0;
         }
 }
