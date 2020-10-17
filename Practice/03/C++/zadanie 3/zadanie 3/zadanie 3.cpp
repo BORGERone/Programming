a2d0576d50265958bdfaf14c0664584e3b2f0f95
BORGER <borger-2018@mail.ru>
2020-10-17 10:22:54 +0300

    commitayou

---
+++ Practice/03/C++/zadanie 3/zadanie 3/zadanie 3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	double d;
+	double c;
+	int a;
+	int b;
+
+	cout << "Введите 2 целых числа\n";
+		cin >> a;
+		cin >> b;
+		cout << "Введите 2 не целых числа\n";
+		cin >> c;
+		cin >> d;
+
+		cout << "a + b = " << a + b << endl;
+		cout << "a - b = " << a - b << endl;
+		cout << "a * b = " << a * b << endl;
+		cout << "a / b = " << a / b << endl;
+		//целые числа
+		cout << "c + d = " << c + d << endl;
+		cout << "c - d = " << c - d << endl;
+		cout << "c * d = " << c * d << endl;
+		cout << "c / d = " << c / d << endl;
+		//дробные числа
+		cout << "c + b = " << c + b << endl;
+		cout << "c - b = " << c - b << endl;
+		cout << "c * b = " << c * b << endl;
+		cout << "c / b = " << c / b << endl;
+		//дробные с целыми
+		cout << "a + d = " << a + d << endl;
+		cout << "a - d = " << a - d << endl;
+		cout << "a * d = " << a * d << endl;
+		cout << "a / d = " << a / d << endl;
+		//целые с дробными
+
+		//С++ выдает целые числа при переменной типа int, но если есть хотя бы одна переменная типа double - получается дробное число
+}
---
+++ Practice/05/C++/zadanie 5/zadanie 5/zadanie 5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+double x(double x0, double v0, double a, double t) {
+	return x0 + v0 * t - (a * t * t) / 2;
+}
+
+	int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x0;
+	double v0;
+	double t;
+	double g = 9.8;
+
+	cout << "Введите x0, v0 и t через пробел\n" ;
+	cin >> x0 >> v0 >> t;
+	double xt = abs(x0 - x(x0, v0, g, t));
+	cout << "xt = " << xt;
+}
