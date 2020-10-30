ef4e87062d72ef25d7713471e2010efa64f1c063
BORGER <borger-2018@mail.ru>
2020-10-30 12:05:44 +0300

    ia bil na pare

---
+++ Practice/13/C++/zadanie 13/zadanie 13/zadanie 13.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+    int a;
+    cout << "Введите число:\n ";
+    cin >> a;
+    if (a == 1 || a % 2 == 0 && a / 2 != 0)
+        cout << "Составное " << endl;
+    else
+        cout << "Простое\n" << endl;
+}
---
+++ Practice/13/Python/zadanie 13p/zadanie 13p/zadanie_13p.py
@@ -0,0 +1,6 @@
+print ("Введите число")
+a = int(input())
+if (a == 1 or a % 2 == 0 and a / 2 != 0):
+    print("Составное")
+else:
+ print("Простое")
